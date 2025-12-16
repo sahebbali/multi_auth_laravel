9171f3ef676584586f61d3e50c4c80c9af9c31db
sahebbali <saheb1996jmr@gmail.com>
2025-12-16 16:44:34 +0600

    add admin and vendor controller

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,14 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+         $user = Auth::user();
+        if ($user->role == 'admin') {
+            return redirect()->intended(route('admin.dashboard', absolute: false));
+        } elseif ($user->role == 'vendor') {
+            return redirect()->intended(route('vendor.dashboard', absolute: false));
+        } else {
+            return redirect()->intended(route('dashboard', absolute: false));
+        }
     }
 
     /**
