8a58f6c259a22458892c0f4c6acc5fdd056e2462
sahebbali <saheb1996jmr@gmail.com>
2025-12-16 16:29:15 +0600

    add admin and vandor route

---
+++ routes/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminDashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->group(function () {
+
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+    });
+});
---
+++ routes/vendor.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Vendor\VendorDashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('vendor')->group(function () {
+
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/dashboard', [VendorDashboardController::class, 'index'])->name('vendor.dashboard');
+    });
+});
